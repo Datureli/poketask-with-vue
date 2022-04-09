64b74739e8528386a71ef02a415b91ff877e728a
Datureli <soleris1@gmail.com>
2022-04-09 13:15:13 +0200

    add validation in home component

---
+++ src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Welcome</h1>
+    <p>What is your name?</p>
+    <div class="buttonGroup">
+      <label for="name">
+        <input type="text" placeholder="your nickname" v-model="nickname" />
+      </label>
+      <button>continue</button>
+      <div class="error" v-if="error">
+        {{ error }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+export default {
+  setup() {
+    let nickname = ref(null);
+    const error = computed(() => {
+      return nickname.value === "" ? "The nickname is required" : "";
+    });
+    return { nickname, error };
+  },
+};
+</script>
+
+<style>
+.buttongroup {
+  width: 300px;
+  display: grid;
+  margin: auto;
+}
+.error {
+  margin-top: 5px;
+  color: #ff0000;
+}
+</style>
