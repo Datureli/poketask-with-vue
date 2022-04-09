c851f305e62a9d9601cb19d8469e18d08cc0102f
Datureli <soleris1@gmail.com>
2022-04-09 19:01:57 +0200

    add axios to project

--- src/components/UserInterface.vue
+++ src/components/UserInterface.vue
@@ -1,5 +1,25 @@
 <template>
-    <div>
-        dsadsa
-    </div>
+  <div>
+    <h1>Welcome to your dashboard {{ welcome }}</h1>
+  </div>
 </template>
+
+<script>
+import { computed } from "vue";
+export default {
+  props: {
+    nickname: {
+      type: String,
+    },
+  },
+  setup(props) {
+    let welcome = computed(() => {
+      return props.nickname;
+    });
+
+    return {
+      welcome,
+    };
+  },
+};
+</script>
