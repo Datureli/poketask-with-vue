d4e994519f45abc2948d84508357ad0c7c99ff4b
Datureli <soleris1@gmail.com>
2022-04-10 17:02:45 +0200

    add randomize catch'em button

--- src/components/Pokemons.vue
+++ src/components/Pokemons.vue
@@ -3,14 +3,19 @@
     <ul>
       <li v-for="(pokemon, index) in pokemons" :key="index">
         <h2>{{ pokemon.name }}</h2>
-        <img :src="imageUrl + pokemon.id + '.png'" alt="dsada" />
+        <img :src="imageUrl + pokemon.id + '.png'" alt="pokemon image" />
+        <p v-if="Math.random() < 0.5">{{ succesCatch }}</p>
+        <p v-else>{{ failCatch }}</p>
       </li>
     </ul>
+
+    <button @click="tryCatch">Catch'em</button>
+      <p style="color: transparent">{{ successOrNot }}</p>  
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from "vue";
+import { reactive, toRefs, computed } from "vue";
 
 export default {
   name: "Home",
@@ -20,6 +25,10 @@ export default {
       imageUrl:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",
       nextUrl: "",
+      succesCatch: "Congratulation!",
+      failCatch: "failure!",
+      successOrNot: null,
+      randomCatch: ["Congratulation", "failure"],
     });
 
     fetch("https://pokeapi.co/api/v2/pokemon?limit=5&offset=[50]")
@@ -34,10 +43,19 @@ export default {
               return !!url;
             })
             .pop();
+
           state.pokemons.push(pokemon);
         });
       });
-    return { ...toRefs(state) };
+
+    const tryCatch = () => {
+      state.successOrNot = Math.floor(Math.random() * state.randomCatch.length);
+    };
+    const catchResult = computed(() => {
+      return state.randomCatch[state.successOrNot] || null;
+    });
+
+    return { ...toRefs(state), catchResult, tryCatch };
   },
 };
 </script>
@@ -56,6 +74,7 @@ li {
   width: 200px;
   height: 200px;
   border: 2px solid rgb(121, 121, 131);
+  text-transform: capitalize;
 }
 img {
   display: flex;
